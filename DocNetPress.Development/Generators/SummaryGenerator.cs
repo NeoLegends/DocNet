ac55c5308d2757ebe9c45d46179247b290561015
Moritz Gunz <moritz.gunz@googlemail.com>
2013-05-16 22:15:27 +0200

    Work

---
+++ DocNetPress.Development/Generators/SummaryGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocNetPress.Generators
+{
+    public class SummaryElement : IPageElement
+    {
+        public String GetPageContent(String xmlFile, String memberXmlNode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

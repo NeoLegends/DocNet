db3c0ea602b86d5db4b3bf68928f33b9fa32ddaa
Moritz Gunz <moritz.gunz@googlemail.com>
2013-05-16 19:04:28 +0200

    Added ZWÄG to PostGenerator
    - Made it modular
    - Removed some useless hardcoded shit

---
+++ DocNetPress.Development/Generators/IPageElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocNetPress.Generators
+{
+    /// <summary>
+    /// Allows an object to be used as page element generator for DocNetPress
+    /// </summary>
+    public interface IPageElement
+    {
+        /// <summary>
+        /// Generates HTML-Code from the given assembly member ready to insert into the post content
+        /// </summary>
+        /// <remarks>
+        /// Do not include breaks or any kind of formatting at the beginning of the result HTML, the <see cref="DocNetPress.Generators.PostGenerator"/>
+        /// will take care of the space between the page elements automatically. If your generator is not able to process the given input, return null.
+        /// </remarks>
+        /// <param name="xmlFile">The path to the XML-File containing the assembly documentation</param>
+        /// <param name="memberXmlNode">The node / member name whose documentation shall be generated</param>
+        /// <returns>The generated documentation HTML-Code ready to insert into the post content</returns>
+        public String GetPageContent(String xmlFile, String memberXmlNode);
+    }
+}
---
+++ DocNetPress.Development/Generators/PostGenerator.cs
@@ -0,0 +1,129 @@
+﻿using DocNetPress.XmlRpc.Posts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocNetPress.Generators
+{
+    /// <summary>
+    /// Generates WordPress-Pages from a given .NET-XML-Documentation file
+    /// </summary>
+    [Serializable]
+    public class PostGenerator
+    {
+        /// <summary>
+        /// The path to the documentation file
+        /// </summary>
+        public String DocumentationFile { get; set; }
+
+        /// <summary>
+        /// Backing field for PropertyPageElements
+        /// </summary>
+        private readonly List<IPageElement> _PageElements = new List<IPageElement>();
+
+        /// <summary>
+        /// List containing all <see cref="DocNetPress.Generators.IPageElement"/>s responsible for property pages
+        /// </summary>
+        public List<IPageElement> PageElements
+        {
+            get
+            {
+                return _PageElements;
+            }
+        }
+
+        /// <summary>
+        /// Backing field for PostType
+        /// </summary>
+        private PostType _PostType = PostType.Page;
+
+        /// <summary>
+        /// Whether the generated post will be a page or a post
+        /// </summary>
+        public PostType PostType
+        {
+            get
+            {
+                return _PostType;
+            }
+            set
+            {
+                _PostType = value;
+            }
+        }
+
+        /// <summary>
+        /// If we're dealing with a custom post type, insert it's name here
+        /// </summary>
+        public String CustomPostTypeName { get; set; }
+
+        /// <summary>
+        /// Locks this class instance
+        /// </summary>
+        private object lockObject = new object();
+
+        /// <summary>
+        /// Initializes an empty <see cref="DocNetPress.Generators.PostGenerator"/>-Instance
+        /// </summary>
+        public PostGenerator()
+        {
+            this.InitializeDefaultPageStructure();
+        }
+
+        /// <summary>
+        /// Initializes a new <see cref="DocNetPress.Generators.PostGenerator"/>-Instance and sets the path to the documentation file
+        /// </summary>
+        /// <param name="documentationFileName">The path to the documentation file read by the <see cref="DocNetPress.Generators.PostGenerator"/></param>
+        public PostGenerator(String documentationFileName) : this()
+        {
+            this.DocumentationFile = documentationFileName;
+        }
+
+        /// <summary>
+        /// Initializes a new <see cref="DocNetPress.Generators.PostGenerator"/>-Instance and sets the path to the documentation file and the post type
+        /// </summary>
+        /// <param name="documentationFileName">The path to the documentation file read by the <see cref="DocNetPress.Generators.PostGenerator"/></param>
+        /// <param name="postType">Whether the generator shall generate WordPress pages, posts or an entirely custom taxonomy</param>
+        public PostGenerator(String documentationFileName, PostType postType)
+            : this(documentationFileName)
+        {
+            this.PostType = postType;
+        }
+
+        /// <summary>
+        /// Initializes a new <see cref="DocNetPress.Generators.PostGenerator"/>-Instance and sets the path to the documentation file, the post type and
+        /// the custom taxonomy name
+        /// </summary>
+        /// <param name="documentationFileName">The path to the documentation file read by the <see cref="DocNetPress.Generators.PostGenerator"/></param>
+        /// <param name="postType">Whether the generator shall generate WordPress pages, posts or an entirely custom taxonomy</param>
+        /// <param name="customPostTypeName">The name of the taxonomy to insert the posts into</param>
+        public PostGenerator(String documentationFileName, PostType postType, String customPostTypeName)
+            : this(documentationFileName, postType)
+        {
+            this.CustomPostTypeName = customPostTypeName;
+        }
+
+        /// <summary>
+        /// Extracts the XML file and generates <see cref="DocNetPress.XmlRpc.Posts.Post"/>-Instances from the documentation ready to push
+        /// to a WordPress installation
+        /// </summary>
+        /// <returns>The generates <see cref="DocNetPress.XmlRpc.Posts.Post"/>s containing a well-formatted documentation</returns>
+        public PostInsert[] GeneratePosts()
+        {
+            lock (lockObject)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Sets the default page structure
+        /// </summary>
+        private void InitializeDefaultPageStructure()
+        {
+            
+        }
+    }
+}

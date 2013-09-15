fe5519d475d6f1991724e71edd4672e8a851bcfc
Moritz Gunz <moritz.gunz@googlemail.com>
2013-09-15 20:09:06 +0200

    Work

---
+++ DocNetPress.Development/WordPressHttpTransmitter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DocNetPress.Development
+{
+    internal class WordPressHttpTransmitter
+    {
+    }
+}
